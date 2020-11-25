136a2aa7ebdf47ed5c1b21c12fa455c76daf2943
Ayyah, Aparna Lakshmi <aayyah2@illinois.edu>
2020-11-24 22:29:07 -0600

    Documenting data importation

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #include <fstream>
 
 int main() {
+  //code for importing airportData (file_to_string is in other.cpp)
   const std::string & airportData = "/home/cgurna2/aditiaa2-aayyah2-jchint2-cgurna2/airportData.csv";
   file_to_string(airportData);
 }
