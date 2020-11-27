df7e068a9426b3c25698f21d7267fbb4719a6e8b
aparnaayyah <aayyah2@illinois.edu>
2020-11-27 15:30:02 -0600

    test suite

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,1 +1,16 @@
+#include "catch/catch.hpp"
 
+#include "other.h"
+#include "BFS.h"
+
+#include <iostream>
+
+
+TEST_CASE("Main properly accesses 3 digit code") {
+  const std::string & airportData = "airportData.csv";
+  std::vector<std::string> data = file_to_vector(airportData);
+  std::vector<array<string, 3>> refinedData = parseVector(data, data.size());
+
+  bool actual = airportFound("GKA", refinedData);
+  REQUIRE(actual);
+}
