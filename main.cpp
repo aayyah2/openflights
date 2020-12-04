7362f6a4381a337d85b9309b9afcc119c9a6aa67
Joshmita Chintala <jchint2@illinois.edu>
2020-12-04 11:14:29 -0600

    adding main.cpp

--- main.cpp
+++ main.cpp
@@ -19,24 +19,15 @@ using namespace std;
 int main() {
   
   //code for importing airportData (file_to_string is in other.cpp)
-<<<<<<< HEAD
   
-=======
-
->>>>>>> e120cb2388823f322e4ade73713f8554a2875159
   
   const std::string & airportData = "Datasets/airportData.csv";
   std::vector<std::string> data = file_to_vector(airportData);
   //parse through the vector to return needed info (3 digit code, lat, long)
   array<int, 3> params{4,6,7};
   std::vector<array<string, 3>> refinedData = parseVector(data, data.size(), params);
-<<<<<<< HEAD
   
   
-=======
-
-
->>>>>>> e120cb2388823f322e4ade73713f8554a2875159
   const std::string & airportRoutesData = "Datasets/airportRoutesData.csv";
   std::vector<std::string> routesData = file_to_vector(airportRoutesData);
   array<int, 3> paramsTwo{2,4,7};
@@ -45,15 +36,9 @@ int main() {
   array<string, 3> routeOne = routes[0];
   //cout << routeOne[0] << routeOne[1] << routeOne[2] << endl;
   Graph g(true, true);
-<<<<<<< HEAD
   
   int i = 0;
   
-=======
-
-  int i = 0;
-
->>>>>>> e120cb2388823f322e4ade73713f8554a2875159
   std::vector<Vertex> addedVertexes;
   
   for (array<string, 3> route : routes) {
@@ -78,31 +63,14 @@ int main() {
     }
     i++;
   }
-<<<<<<< HEAD
   
   g.initSnapshot("Out");
   g.snapshot();
   
   
   Dijkstra shortestPath(g, "CEK", "KZN");
-=======
-
-  g.initSnapshot("Out");
-  g.snapshot();
   
-
-  Dijkstra shortestPath(g, "CEK", "KZN");
- 
->>>>>>> e120cb2388823f322e4ade73713f8554a2875159
   
-  for (auto pair : shortestPath.distances) {
-    
-      cout << pair.first<< ":" << pair.second << endl;
-    
-    
-  }
-  
-<<<<<<< HEAD
   for (auto pair : shortestPath.distances) {
     
     cout << pair.first<< ":" << pair.second << endl;
@@ -115,40 +83,26 @@ int main() {
   
   
   /**
-=======
-
- //shortestPath.printItinerary();
-
-
-/**
-
->>>>>>> e120cb2388823f322e4ade73713f8554a2875159
-  for (int i = 0; i < 10; i++) {
-  //array<string, 3> temp{refinedData[i]};
-  cout<<refinedData[i][0]<<endl;
-  }
-  
-  string departure = "";
-  cout<< "Enter Departure Airport Code With Quotations: ";
-  cin>> departure;
-  
-  cout << airportFound(departure, refinedData) <<endl;
-  string arrival = "";
-  cout<< "Enter Arrival Airport Code: ";
-  cin>> arrival;
-  cout<<"Calculating path from " << departure<< " to " << arrival<< endl;
-  //try catch to ensure valid airports
-  if (airportFound(departure, refinedData)&& airportFound(arrival,refinedData)) {
-  //check for accuracy
-  cout << findDistance(arrival, departure, refinedData) << endl;
-  }
-  
-<<<<<<< HEAD
-  **/
+   for (int i = 0; i < 10; i++) {
+   //array<string, 3> temp{refinedData[i]};
+   cout<<refinedData[i][0]<<endl;
+   }
+   
+   string departure = "";
+   cout<< "Enter Departure Airport Code With Quotations: ";
+   cin>> departure;
+   
+   cout << airportFound(departure, refinedData) <<endl;
+   string arrival = "";
+   cout<< "Enter Arrival Airport Code: ";
+   cin>> arrival;
+   cout<<"Calculating path from " << departure<< " to " << arrival<< endl;
+   //try catch to ensure valid airports
+   if (airportFound(departure, refinedData)&& airportFound(arrival,refinedData)) {
+   //check for accuracy
+   cout << findDistance(arrival, departure, refinedData) << endl;
+   }
+   
+   **/
   
 }
-=======
-**/
-
-}
->>>>>>> e120cb2388823f322e4ade73713f8554a2875159
