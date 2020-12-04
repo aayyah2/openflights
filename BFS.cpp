8c2c94f610539f0c7c27edffee8a844b2d8af2a0
Joshmita Chintala <jchint2@illinois.edu>
2020-12-04 16:01:17 -0600

    adding bfs

--- BFS.cpp
+++ BFS.cpp
@@ -1,14 +1,53 @@
 #include <iterator>
 #include <cmath>
-
+#include <iostream>
 #include <list>
 #include <queue>
 #include <stack>
 #include <vector>
 
 #include "BFS.h"
+using namespace std;
 
-
-BFS::BFS() {  
-  
+BFS::BFS(int V) 
+{  
+    this->V = V; 
+    adj = new list<int>[V]; 
 }
+
+void BFS::addEdge(int v, int w) 
+{ 
+    adj[v].push_back(w); 
+} 
+  
+void BFS::BFSS(int s) 
+{ 
+    bool *visited = new bool[V]; 
+    for(int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    list<int> queue; 
+  
+    visited[s] = true; 
+    queue.push_back(s); 
+  
+
+    list<int>::iterator i; 
+  
+    while(!queue.empty()) 
+    { 
+        s = queue.front(); 
+        cout << s << " "; 
+        queue.pop_front(); 
+  
+       
+        for (i = adj[s].begin(); i != adj[s].end(); ++i) 
+        { 
+            if (!visited[*i]) 
+            { 
+                visited[*i] = true; 
+                queue.push_back(*i); 
+            } 
+        } 
+    } 
+} 
