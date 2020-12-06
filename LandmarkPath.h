4d7c1bbb09862b66229f591e339b2040535b1c9c
Adya, Aditi <aditiaa2@illinois.edu>
2020-12-06 13:43:52 -0600

    comments

--- LandmarkPath.h
+++ LandmarkPath.h
@@ -1,12 +1,13 @@
 #include "Dijkstra.h"
 
+//function for taking Djikstra and inserting airports
 class LandmarkPath {
     public:
-        LandmarkPath(Graph g, Vertex origin, Vertex destination, Vertex layover);
-        std::vector<Vertex> layoverPath;
+        LandmarkPath(Graph g, Vertex origin, Vertex destination, Vertex layover); //similar to Djikstra function
+        std::vector<Vertex> layoverPath; //the path that is takes to get from starting airport to ending airport
         void printLayoverItinerary();
-        Vertex pointOne;
-        Vertex pointTwo;
-        Vertex pointThree;
-        double totalDistance;
+        Vertex pointOne; //starting 
+        Vertex pointTwo; //layover
+        Vertex pointThree; //ending
+        double totalDistance; //total distance that is calculated
 };
