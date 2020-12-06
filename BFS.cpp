a3d7ace4506fe435805381262c8d6b3f1c1ecdcc
Adya, Aditi <aditiaa2@illinois.edu>
2020-12-06 12:58:22 -0600

    comments

--- BFS.cpp
+++ BFS.cpp
@@ -9,13 +9,17 @@
 #include <map>
 using namespace std;
 
-BFS::BFS(Graph g, Vertex origin) //g is the graph, s is the source node
+BFS::BFS(Graph g, Vertex origin) //g is the graph, origin is the source node (starting airport)
 {  
-   vector<Vertex> bfsNodes;
-    //visited := a set to store references to all visited nodes
+    vector<Vertex> bfsNodes; //vector of vertices named bfsNodes
+   
+    //get vertices from our particular dataset
     vector<Vertex> vertices = g.getVertices();
+   
+    //visited := a set to store references to all visited nodes
     std::map<Vertex, bool> visited;
 
+    //setting all vertices as not visited 
     for (Vertex v : vertices) {
         visited[v] = false;
     }
@@ -30,13 +34,14 @@ BFS::BFS(Graph g, Vertex origin) //g is the graph, s is the source node
     visited[origin] = true;
 
     Vertex current;
+    
     //while queue is not empty:
     while (q.size() > 0) {
         //process current_node
         current = q.front();
-        bfsNodes.push_back(current); 
+        bfsNodes.push_back(current); //enqueueing the current node from the vertices given
         
-        vector<Vertex> currentAdjacent = g.getAdjacent(current);
+        vector<Vertex> currentAdjacent = g.getAdjacent(current); //finding the adjacent nodes
         
         //delete current node so that we can process next nodes
         //cout << q.front() << endl;
@@ -57,10 +62,8 @@ BFS::BFS(Graph g, Vertex origin) //g is the graph, s is the source node
             }
         }
     }
-
+    //print the vertex node and moving to next line
     for (Vertex node: bfsNodes) {
         cout << node << endl;
     }
-    
-
 }
