486054e1bbad60c01087d88469408920ba491514
Adya, Aditi <aditiaa2@illinois.edu>
2020-12-06 13:37:04 -0600

    comments

--- Dijkstra.h
+++ Dijkstra.h
@@ -17,17 +17,17 @@ using namespace std;
 class Dijkstra {
     public :
         //constructor for shortest path finder
-        Dijkstra(Graph g, Vertex origin, Vertex destination);
-        void printItinerary();
-        void BFSAttempt(Graph g, Vertex origin);
+        Dijkstra(Graph g, Vertex origin, Vertex destination); 
+        void printItinerary(); //printing the itinerary
+        void BFSAttempt(Graph g, Vertex origin); //BFS for Djikstra
 
-        Vertex startingPoint;
-        Vertex endingPoint;
+        Vertex startingPoint; //starting point vertex
+        Vertex endingPoint; //ending point vertex
 
-        std::vector<Vertex> q;
-        std::vector<Vertex> myJourney;
-        std::map<string, double> distances;
-        std::map<string, vector<Vertex>> paths;
-        double travelDistance;
+        std::vector<Vertex> q; //holds all vertices or airport
+        std::vector<Vertex> myJourney; //the itinerary of all paths
+        std::map<string, double> distances; //distance from airport to airport
+        std::map<string, vector<Vertex>> paths; //path including any layovers if needed
+        double travelDistance; //total distance travelled
 
 };
